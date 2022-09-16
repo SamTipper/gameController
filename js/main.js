ec9cf7bfabeca9947a61eda27cfb827711a8ebf8
SamTipper <samtipperprogrammer@gmail.com>
2022-09-16 19:21:37 +0100

    Save API Key in localStorage

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
 const Http = new XMLHttpRequest();
-Http.open("GET", "https://conga.coombszy.com/health");
-Http.send();
-Http.onreadystatechange = function(){
-    if (this.readyState !== 4 && this.status !== 200){
-        alert("Cannot connect to server, please try again later.")
-        location.reload();
+    Http.open("GET", "https://conga.coombszy.com/health");
+    Http.send();
+    Http.onreadystatechange = function(){
+        if (this.readyState !== 4 && this.status !== 200){
+            alert("Cannot connect to server, please try again later.")
+            location.reload();
+        }
     }
-}
-const HttpAuth = new XMLHttpRequest();
-const apiKey = prompt("Please Enter API Key");
-if (apiKey == null){
-    location.reload();
-}
-HttpAuth.open("GET", "https://conga.coombszy.com/auth");
-HttpAuth.setRequestHeader("Authorization", apiKey);
-HttpAuth.send();
-HttpAuth.onreadystatechange = function(){
-    if (this.status !== 204){
-        alert("API Key Incorrect.")
-        location.reload();
-    }
-}
-if (localStorage.key()){
-    
-}
 
+if (localStorage.getItem("apiKey") === null){
+    const HttpAuth = new XMLHttpRequest();
+    const apiKey = prompt("Please Enter API Key");
+    if (apiKey == null){
+        location.reload();
+    }
+    HttpAuth.open("GET", "https://conga.coombszy.com/auth");
+    HttpAuth.setRequestHeader("Authorization", apiKey);
+    HttpAuth.send();
+    HttpAuth.onreadystatechange = function(){
+        if (this.status !== 204){
+            alert("API Key Incorrect.")
+            location.reload();
+        }
+    }
+    localStorage.setItem("apiKey", apiKey)
+}else{
+    const HttpAuth = new XMLHttpRequest();
+    HttpAuth.open("GET", "https://conga.coombszy.com/auth");
+    HttpAuth.setRequestHeader("Authorization", localStorage.getItem("apiKey"));
+    HttpAuth.send();
+    HttpAuth.onreadystatechange = function(){
+        if (this.status !== 204){
+            alert("API Key Outdated.")
+            localStorage.removeItem("apiKey")
+            location.reload();
+        }
+    }
+}
 
 function key_press(key){
     let request = {
