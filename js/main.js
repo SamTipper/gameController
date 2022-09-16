6b7b121daebc19ec277ff60fbdbe3a6ed666f166
SamTipper <samtipperprogrammer@gmail.com>
2022-09-16 19:06:22 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const Http = new XMLHttpRequest();
+Http.open("GET", "https://conga.coombszy.com/health");
+Http.send();
+Http.onreadystatechange = function(){
+    if (this.readyState !== 4 && this.status !== 200){
+        alert("Cannot connect to server, please try again later.")
+        location.reload();
+    }
+}
+const HttpAuth = new XMLHttpRequest();
+const apiKey = prompt("Please Enter API Key");
+if (apiKey == null){
+    location.reload();
+}
+HttpAuth.open("GET", "https://conga.coombszy.com/auth");
+HttpAuth.setRequestHeader("Authorization", apiKey);
+HttpAuth.send();
+HttpAuth.onreadystatechange = function(){
+    if (this.status !== 204){
+        alert("API Key Incorrect.")
+        location.reload();
+    }
+}
+if (localStorage.key()){
+    
+}
+
+
+function key_press(key){
+    let request = {
+        "queue": "ahK34NvA",
+        "content": {
+            "keyPress": key
+        }
+    };
+    const HttpSend = new XMLHttpRequest();
+    HttpSend.open("POST", "https://conga.coombszy.com/item");
+    HttpSend.setRequestHeader("Authorization", apiKey);
+    HttpSend.setRequestHeader("Content-Type", "application/json");
+    HttpSend.send(JSON.stringify(request));
+}
